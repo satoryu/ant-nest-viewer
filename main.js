fa56800afa47cadd22ceb89f1a7e52a7b697c7de
Tatsuya Sato <satoryu.1981@gmail.com>
2022-05-20 08:30:15 +0900

    Remove a cube, loads glb of ant-nets and install a controller

--- main.js
+++ main.js
@@ -1,27 +1,42 @@
 import './style.css'
 
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const ambientLight = new THREE.AmbientLight(0xF04040);
+const directionalLight = new THREE.DirectionalLight(0xF04040);
+directionalLight.position.set(1, 2, 0);
+scene.add(ambientLight);
+scene.add(directionalLight);
+camera.position.z = 2.5;
+console.log(camera.position)
+camera.updateProjectionMatrix();
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
-camera.position.z = 5;
+new OrbitControls(camera, renderer.domElement);
 
 function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
-
-  cube.rotation.x += 0.03;
-  cube.rotation.y += 0.01;
 }
 
-animate();
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+const loader = new GLTFLoader();
+const gltfUrl = new URL('./gltf/82.glb', import.meta.url).href;
+
+loader.load(gltfUrl,
+  function(gltf) {
+    scene.add(gltf.scene);
+    animate();
+  },
+  undefined,
+  function(error) {
+    console.error(error)
+  }
+)
